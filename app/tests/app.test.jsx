3a62c3a418faf7131e9766cc4ef3d36ff5cfce23
Vineet Kapoor <vkapo001@ucr.edu>
2016-12-31 02:38:37 -0500

    First commit

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,9 @@
+var expect = require('expect');
+
+
+//Describe is a strictly visual function. It only helps in visually grouping the tests 
+describe('Test Experiments', () => {
+	it('should properly run tests', function() {
+		expect(1).toBe(1);
+	});
+});
